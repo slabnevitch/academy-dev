2561cee2faab0e6c69864c4fe09155be776af1d5
slabnevitch <handofschpilrain@mail.ru>
2019-07-02 21:59:07 +0500

    experiences section ready

--- js/common.js
+++ js/common.js
@@ -1,8 +1,84 @@
 $(function() {
 
-	$('.menu-icon').click(function(){
-		$(this).toggleClass('clicked');
-	});
+	// section Header
+		$('.menu-icon').click(function(){
+			$(this).toggleClass('clicked');
+		});
+	
+	// end section Header
+
+	// section we-work
+
+		(function howWorkdHandler(index) {
+			var $counter = $('.we-work-informer__counter'),
+				$tabs = $('.tabs__link'),
+				$informerTextItems = $('.we-work-informer__text .tabs__item'),
+				$buttons = $('.we-work-informer__handler .tabs__list .tabs__item'),
+				timer,
+				index  = 0,
+				_self = this;
+
+			this.init = function() {
+				this.events();
+				this.timer();
+			},
+
+			this.events = function() {
+				console.log('events!');
+				$tabs.on('click', this.tabsClick);
+			},
+			this.tabsClick = function(e) {
+				e.preventDefault();
+				var $th = $(this),
+					thIndex = $tabs.index($th);
+
+				_self.indexProgress(thIndex);
+				console.log('index on tabs ' + thIndex);
+			},
+
+			this.timer = function() {
+				timer = setInterval(_self.indexProgress, 5000);
+			},
+			
+			this.indexProgress = function(transitionedIndex) {
+				console.log('transitionedIndex ' + transitionedIndex);
+				if(transitionedIndex !== undefined){
+					console.log('transitionedIndex is defined!');
+					index = transitionedIndex;
+				}else{
+					
+					index++;
+					if(index > 3){
+						index = 0;
+					}	
+				}
+				console.log('index on exit ' + index);
+				this.counterRender();
+				this.buttonsRender();
+				this.textContentRender();
+			},
+			this.counterRender = function() {
+				// console.log(index);
+				$counter.text(+(index + 1));
+			},
+			this.buttonsRender = function() {
+				$buttons.eq(index).addClass('tabs__item--active')
+					.siblings()
+					.removeClass('tabs__item--active');
+			},
+			this.textContentRender = function() {
+				$informerTextItems.eq(index)
+					.removeClass('hidden')
+					.siblings()
+					.addClass('hidden');
+			}
+
+			// this.init();
+		})();
+		
+	// end section we-work
+
+
 	//E-mail Ajax Send
 	//Documentation & Example: https://github.com/agragregra/uniMail
 	$("form").submit(function() { //Change
